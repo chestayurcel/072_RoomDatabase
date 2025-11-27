04547ed2df2672222cf3df92122c7bef52bd4b59
Yurcell <chestayurcel@gmail.com>
2025-11-27 13:32:55 +0700

    menambahkan object DestinasiEntry untuk rute navigasi input data siswa

--- app/src/main/java/com/example/praktikumroomdb/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/praktikumroomdb/view/route/DestinasiEntry.kt
@@ -1,2 +1,8 @@
 package com.example.praktikumroomdb.view.route
 
+import com.example.praktikumroomdb.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route: "item_entry"
+    override val titleRes = R.string.entry_siswa
+}

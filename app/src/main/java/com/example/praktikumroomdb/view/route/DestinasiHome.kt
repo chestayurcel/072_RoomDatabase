9036ef39690591342b6b545652087f264d2ea6d2
Yurcell <chestayurcel@gmail.com>
2025-11-27 13:31:06 +0700

    Menambahkan object DestinasiHome sebagai implementasi DestinasiNavigasi

--- app/src/main/java/com/example/praktikumroomdb/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/praktikumroomdb/view/route/DestinasiHome.kt
@@ -1,2 +1,8 @@
 package com.example.praktikumroomdb.view.route
 
+import com.example.praktikumroomdb.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

4336490f299de7d3ea0c198c89f61291b8d5f833
Yurcell <chestayurcel@gmail.com>
2025-11-27 13:14:47 +0700

    Format ulang kode pada RepositoriSiswa.kt untuk meningkatkan keterbacaan

--- app/src/main/java/com/example/praktikumroomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikumroomdb/repositori/RepositoriSiswa.kt
@@ -12,6 +12,8 @@ interface RepositoriSiswa {
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ): RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
 }

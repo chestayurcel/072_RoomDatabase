cf252a9067879a3411761349afe90bca30aec0bd
Yurcell <chestayurcel@gmail.com>
2025-11-27 13:14:00 +0700

    Mengimplementasikan interface RepositoriSiswa dan kelas OfflineRepositoriSiswa untuk operasi database

--- app/src/main/java/com/example/praktikumroomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikumroomdb/repositori/RepositoriSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.praktikumroomdb.repositori
 
+import com.example.praktikumroomdb.room.Siswa
+import com.example.praktikumroomdb.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}

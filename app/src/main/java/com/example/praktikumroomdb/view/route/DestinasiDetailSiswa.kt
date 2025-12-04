d3b3e81b006db03252f63043837f5edae67a80e0
Yurcell <chestayurcel@gmail.com>
2025-12-04 11:50:18 +0700

    Menambahkan objek DestinasiDetailSiswa untuk navigasi halaman detail siswa

---
+++ app/src/main/java/com/example/praktikumroomdb/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.praktikumroomdb.view.route
+
+import com.example.praktikumroomdb.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
